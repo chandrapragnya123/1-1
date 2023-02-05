a09c2b94cf71950541ce8ba2638bc2df89528248
chandrapragnya123 <120377390+chandrapragnya123@users.noreply.github.com>
2023-02-06 04:51:20 +0530

    Add files via upload

---
+++ prime/prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,count=0;
+	printf("enter a number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			count++;
+		}
+	}
+	if(count==2)
+	printf("the given number is a prime number");
+	else
+	printf("the given number is not a prime number");
+	return 0;
+}
+
+
